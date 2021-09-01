52f31655013b0fd7a986081db42ee0144344959f
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-01 00:38:05 -0700

    lower letter function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - in lowercase
+ *
+ * write a function
+ * 
+ * return void
+ */
+void print_alphabet(void)
+{
+	char lower_letter = 'a';
+
+	while (lower_letter <= 'z')
+	{
+		_putchar(lower_letter);
+		lower_letter++;
+	}
+	_putchar('\n');	
+}
