bcb0be5c39887b0b87538f4becb5395861a8de06
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-01 02:06:27 -0700

    check for alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,9 @@
 
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
