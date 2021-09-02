b945d8e3cdf2e85a91c491e2f7109ae51657cc7c
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-02 01:13:04 -0700

    upercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - Checks for upercase character
+ *@c: integer type
+ *
+ *Return: 1 if upercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
