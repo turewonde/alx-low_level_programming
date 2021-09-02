1bbf3216751cca6052264cfaa6dcfcc108690ac2
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-02 05:47:55 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - Print the numbers from 0 to 9 with out 2 and 4
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+	for (int a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+	}
+	_putchar('\n');
+}
