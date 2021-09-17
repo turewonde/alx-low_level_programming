7b9dada37b8a77655bf15ff42c148d59cf493598
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-17 05:17:52 -0700

    print a name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - print its name
+ *@argc: arguments count
+ *@argv: arguments array
+ *Return: 0 success
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+	       
