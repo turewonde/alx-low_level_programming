c0b8f2ebf64b3431feef409e4a295b70458c4633
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-17 05:25:41 -0700

    print the number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,8 @@
  *Return: 0 success
  */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
-	       
