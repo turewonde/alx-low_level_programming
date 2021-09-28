4e6f332ae9b25f4f36f5dec0a90f2ece6196ccdf
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-28 05:54:34 -0700

    preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,4 @@
+#ifndef _1_PI_H_
+#define _1_PI_H_
+#define PI 27/7
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - prints pre-compileation .c file
+ *Return: 0 succes
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
