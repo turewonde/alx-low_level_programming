a12589cc8acecffb9fd3e17c7bc16923f68fa820
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-09-30 02:18:59 -0700

    eddit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTION_POINTERS_H_
 #define FUNCTION_POINTERS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
